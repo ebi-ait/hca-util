be703dac85889ddac0e50e34a2f3b08cd9524aa7
prabht <prabhat.totoo@gmail.com>
2020-03-11 15:25:50 +0000

    write to a progress list instead of printing to sys.out

--- hca_util/upload_progress.py
+++ hca_util/upload_progress.py
@@ -5,19 +5,20 @@ import threading
 
 class UploadProgress(object):
 
-    def __init__(self, filename):
+    def __init__(self, filename, progress_list, i):
+        self._progress_list = progress_list
+        self._idx = i
         self._filename = filename
         self._size = float(os.path.getsize(filename))
         self._seen_so_far = 0
         self._lock = threading.Lock()
 
     def __call__(self, bytes_amount):
-        # To simplify, assume this is hooked up to a single filename
+
         with self._lock:
             self._seen_so_far += bytes_amount
             percentage = (self._seen_so_far / self._size) * 100
-            sys.stdout.write(
-                "\r%s  %s / %s  (%.2f%%)" % (
-                    self._filename, self._seen_so_far, self._size,
-                    percentage))
-            sys.stdout.flush()
+            # sys.stdout.write("\r%s  %s / %s  (%.2f%%)" % (self._filename, self._seen_so_far, self._size, percentage))
+            # sys.stdout.flush()
+
+            self._progress_list[self._idx] = "%s  %s / %s  (%.2f%%)" % (self._filename, self._seen_so_far, self._size, percentage)
