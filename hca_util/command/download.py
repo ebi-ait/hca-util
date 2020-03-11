c515714084936614a8f4e398a336ce312de8356b
prabht <prabhat.totoo@gmail.com>
2020-03-11 15:32:36 +0000

    reviewed download cmd impl, not including progress indication yet

--- hca_util/command/download.py
+++ hca_util/command/download.py
@@ -1,43 +1,55 @@
-from .command import HcaCmd
+import os
+from hca_util.local_state import get_selected_dir
+from hca_util.common import print_err
 
 
-class CmdDownload(HcaCmd):
+class CmdDownload:
+    """
+    user: both wrangler and contributor
+    aws resource or client used in command - s3 resource (Bucket().upload_file), s3 client list_objects_v2
+    """
 
-    def cmd_download(self, argv):
-        if not self.setup_ok:
-            print(f'Setup failed: \nSee `help config` for help to configure your credentials')
-            return
+    def __init__(self, aws, args):
+        self.aws = aws
+        self.args = args
 
-        if self.selected_dir is None:
+    def run(self):
+
+        selected_dir = get_selected_dir()
+
+        if not selected_dir:
             print('No directory selected')
             return
 
-        if len(argv) < 1:
-            print('Invalid args. See `help download`')
-            return
-
         try:
-            prefix = self.selected_dir
-            s3_resource = self.aws.session.resource('s3')
-            bucket = s3_resource.Bucket(self.bucket_name)
-            if len(argv) == 1 and argv[0] == '.':
+            prefix = selected_dir
+            s3_resource = self.aws.common_session.resource('s3')
+            bucket = s3_resource.Bucket(self.aws.bucket_name)
+
+            # choice 1
+            all_files = self.args.a  # optional bool
+
+            fs = []
+            if all_files:
                 # download all files from selected directory
                 for obj in bucket.objects.filter(Prefix=prefix):
-                    # do not download folder object
+                    # skip the top-level directory
                     if obj.key == prefix:
                         continue
-                    print('Downloading ' + obj.key)
-                    if not os.path.exists(os.path.dirname(obj.key)):
-                        os.makedirs(os.path.dirname(obj.key))
-                    bucket.download_file(obj.key, obj.key)
+                    fs.append(obj.key)
             else:
-                # download specified file(s) from selected directory
-                for f in argv:
-                    print('Downloading ' + prefix + f)
-                    obj = bucket.Object(prefix + f)
-                    if not os.path.exists(os.path.dirname(obj.key)):
-                        os.makedirs(os.path.dirname(obj.key))
-                    obj.download_file(obj.key)
+                # choice 2
+                # download specified file(s) only
+                fs = [f'{prefix}{f}' for f in self.args.f]
+
+            print('Downloading...')
+
+            for f in fs:
+                # todo check if f exists
+                if not os.path.exists(os.path.dirname(f)):
+                    os.makedirs(os.path.dirname(f))
+                bucket.download_file(f, f)
+                print(f)
 
         except Exception as e:
-            print(f'An exception of type {e.__class__.__name__} occurred in cmd config.\nDetail: ' + str(e))
+            print_err(e, 'download')
