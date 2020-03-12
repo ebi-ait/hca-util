0bc47c8d0639ffc97713cbd4cd069233512974d0
Prabhat Totoo <prabhat@ebi.ac.uk>
2020-03-12 23:59:19 +0000

    completed impl with per file progress

--- hca_util/command/upload.py
+++ hca_util/command/upload.py
@@ -1,11 +1,10 @@
 import os
-# from multiprocessing.dummy import Pool
-import threading
-from time import sleep
 from hca_util.local_state import get_selected_dir
 from hca_util.common import print_err
-from hca_util.upload_progress import UploadProgress
+from hca_util.file_transfer import FileTransfer, TransferProgress, transfer
 from hca_util.command.select import key_exists
+from hca_util.settings import DEBUG_MODE
+
 
 """
 Uploading to Upload Service upload area
@@ -37,64 +36,53 @@ class CmdUpload:
             # choice 1
             all_files = self.args.a  # optional bool
 
+            fs = []
             if all_files:
                 # upload files from current directory
-                fs = [f for f in os.listdir('.') if os.path.isfile(f) and not f.startswith('.')]
+                for f in os.listdir('.'):
+                    # only files (not directories) and skip hidden files
+                    if os.path.isfile(f) and not f.startswith('.'):
+                        f_size = os.path.getsize(f)
+                        fs.append(FileTransfer(key=f, size=f_size))
             else:
 
-                # choice 2
-                files = self.args.f  # optional list of <_io.TextIOWrapper name='f1' mode='r' encoding='UTF-8'>
+                # choice 2 upload specified list of files
+                # optional list of <_io.TextIOWrapper name='f1' mode='r' encoding='UTF-8'>
 
-                # upload specified list of files
-                fs = []
-                for f in files:
+                for f in self.args.f :
                     # argparse takes care of path expansion and check if file doesn't exist
-                    fs.append(f.name)
+                    f_size = os.path.getsize(f.name)
+                    fs.append(FileTransfer(key=f.name, size=f_size))
+
+            def upload(idx):
+                try:
+                    fname = fs[idx].key
+                    key = selected_dir + os.path.basename(fname)
+
+                    # creating a new session for each file upload/thread, as it's unclear whether they're
+                    # thread-safe or not
+
+                    sess = self.aws.new_session()
+                    client = sess.client('s3')
+
+                    if not self.args.o and key_exists(client, self.aws.bucket_name, key):
+                        fs[idx].status = 'File exists. Use -o to overwrite.'
+                        fs[idx].complete = True
+                    else:
+                        res = sess.resource('s3')
+                        # upload_file automatically handles multipart uploads via the S3 Transfer Manager
+                        # put_object maps to the low-level S3 API request, it does not handle multipart uploads
+                        res.Bucket(self.aws.bucket_name).upload_file(Filename=fname, Key=key,
+                                                                     Callback=TransferProgress(fs[idx]))
+                except Exception as thread_ex:
+                    fs[idx].status = 'Upload failed.'
+                    fs[idx].complete = True
+                    if DEBUG_MODE:
+                        print_err(thread_ex, 'upload')
 
             print('Uploading...')
 
-            progress_list = fs.copy()
-
-            def upload(idx):
-                f = fs[idx]
-                key = selected_dir + os.path.basename(f)
-
-                # creating a new session for each file upload/thread, as it's unclear whether they're
-                # thread-safe or not
-
-                sess = self.aws.new_session()
-                client = sess.client('s3')
-
-                if not self.args.o and key_exists(client, self.aws.bucket_name, key):
-                    progress_list[idx] = "%s  File exists. Use -o to overwrite." % f
-                    sleep(1)
-                else:
-                    res = sess.resource('s3')
-                    # upload_file automatically handles multipart uploads via the S3 Transfer Manager
-                    # put_object maps to the low-level S3 API request, it does not handle multipart uploads
-                    res.Bucket(self.aws.bucket_name).upload_file(Filename=f, Key=key,
-                                                                 Callback=UploadProgress(f, progress_list, idx))
-
-            # with Pool(12) as p:
-            #    p.map(lambda f: self.upload(f), fs)
-            #    print('Done.')
-
-            # using a thread per file upload instead of threadpool as they are cheap
-
-            ts = []
-            for i in range(0, len(fs)):
-                t = threading.Thread(target=upload, args=(i,))
-                ts.append(t)
-                t.start()
-
-            while [t for t in ts if t.is_alive()]:
-                for p in progress_list:
-                    print(p)
-                sleep(0.5)  # 1 sec
-                if [t for t in ts if t.is_alive()]:
-                    print(f'\033[{len(fs) + 1}A\r')  # go back to start of first progress_list item
-
-            print('Done')
+            transfer(upload, fs)
 
         except Exception as e:
             print_err(e, 'upload')
