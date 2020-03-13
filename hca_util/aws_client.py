62d44fb4a2ce54e8f38ff3e718586b144e0997e2
prabht <prabhat.totoo@gmail.com>
2020-03-13 14:27:30 +0000

    Move key_exists to obj_exists in aws_client

--- hca_util/aws_client.py
+++ hca_util/aws_client.py
@@ -45,3 +45,23 @@ class Aws:
         secret_str = resp['SecretString']
         self.bucket_name = json.loads(secret_str)['s3-bucket']
         return self.bucket_name
+
+    def obj_exists(self, key):
+        """
+        return true if key exists, else false
+        A folder/directory is an s3 object with key <uuid>/
+        Note: s3://my-bucket/folder != s3://my-bucket/folder/
+        Refer to https://www.peterbe.com/plog/fastest-way-to-find-out-if-a-file-exists-in-s3
+        for comparison between client.list_objects_v2 and client.head_object to make this check.
+        Also check https://stackoverflow.com/questions/33842944/check-if-a-key-exists-in-a-bucket-in-s3-using-boto3
+        which suggests using Object.load() - which does a HEAD request, however, HCAContributor doesn't have
+        s3:GetObject permission by default, so this will fail for them.
+        """
+        response = self.new_session().client('s3').list_objects_v2(
+            Bucket=self.bucket_name,
+            Prefix=key,
+        )
+        for obj in response.get('Contents', []):
+            if obj['Key'] == key:
+                return True
+        return False
--- hca_util/command/select.py
+++ hca_util/command/select.py
@@ -15,11 +15,9 @@ class CmdSelect:
     def run(self):
 
         try:
-            s3_client = self.aws.common_session.client('s3')
-            bucket = self.aws.bucket_name
             key = self.args.DIR if self.args.DIR.endswith('/') else f'{self.args.DIR}/'
 
-            if key_exists(s3_client, bucket, key):
+            if self.aws.obj_exists(key):
                 set_selected_dir(key)
                 print('Selected ' + key)
             else:
@@ -27,24 +25,3 @@ class CmdSelect:
 
         except Exception as e:
             print_err(e, 'select')
-
-
-def key_exists(client, bucket, key):
-    """
-    return true if key exists, else false
-    A folder/directory is an s3 object with key <uuid>/
-    Note: s3://my-bucket/folder != s3://my-bucket/folder/
-    Refer to https://www.peterbe.com/plog/fastest-way-to-find-out-if-a-file-exists-in-s3
-    for comparison between client.list_objects_v2 and client.head_object to make this check.
-    Also check https://stackoverflow.com/questions/33842944/check-if-a-key-exists-in-a-bucket-in-s3-using-boto3
-    which suggests using Object.load() - which does a HEAD request, however, HCAContributor doesn't have
-    s3:GetObject permission by default, so this will fail for them.
-    """
-    response = client.list_objects_v2(
-        Bucket=bucket,
-        Prefix=key,
-    )
-    for obj in response.get('Contents', []):
-        if obj['Key'] == key:
-            return True
-    return False
