d84b548c0d2455d2426178233dc71a302f2df0dd
Prabhat Totoo <prabhat@ebi.ac.uk>
2020-03-13 00:00:12 +0000

    re-impl download using file transfer

--- hca_util/command/download.py
+++ hca_util/command/download.py
@@ -1,4 +1,7 @@
 import os
+import botocore
+from hca_util.file_transfer import FileTransfer, TransferProgress, transfer
+from hca_util.settings import DEBUG_MODE
 from hca_util.local_state import get_selected_dir
 from hca_util.common import print_err
 
@@ -22,7 +25,6 @@ class CmdDownload:
             return
 
         try:
-            prefix = selected_dir
             s3_resource = self.aws.common_session.resource('s3')
             bucket = s3_resource.Bucket(self.aws.bucket_name)
 
@@ -32,24 +34,48 @@ class CmdDownload:
             fs = []
             if all_files:
                 # download all files from selected directory
-                for obj in bucket.objects.filter(Prefix=prefix):
+                for obj in bucket.objects.filter(Prefix=selected_dir):
                     # skip the top-level directory
-                    if obj.key == prefix:
+                    if obj.key == selected_dir:
                         continue
-                    fs.append(obj.key)
+                    fs.append(FileTransfer(key=obj.key, size=obj.size))
             else:
                 # choice 2
                 # download specified file(s) only
-                fs = [f'{prefix}{f}' for f in self.args.f]
+
+                for f in self.args.f:
+                    # check if f exists
+                    key = f'{selected_dir}{f}'
+                    try:
+                        obj_summary = s3_resource.ObjectSummary(self.aws.bucket_name, key)
+                        obj_size = obj_summary.size
+                    except botocore.exceptions.ClientError as e:
+                        if e.response['Error']['Code'] == "404":
+                            fs.append(FileTransfer(key=key, status='File not found.', complete=True))
+                        else:
+                            # Something else has gone wrong.
+                            fs.append(FileTransfer(key=key, status='Download error.', complete=True))
+                    else:
+                        fs.append(FileTransfer(key=key, size=obj_size))
+
+            def download(idx):
+                try:
+                    file = fs[idx].key
+                    if not os.path.exists(os.path.dirname(file)):
+                        os.makedirs(os.path.dirname(file))
+
+                    s3 = self.aws.new_session().resource('s3')
+                    s3.Bucket(self.aws.bucket_name).download_file(file, file, Callback=TransferProgress(fs[idx]))
+
+                except Exception as thread_ex:
+                    fs[idx].status = 'Download failed.'
+                    fs[idx].complete = True
+                    if DEBUG_MODE:
+                        print_err(thread_ex, 'download')
 
             print('Downloading...')
 
-            for f in fs:
-                # todo check if f exists
-                if not os.path.exists(os.path.dirname(f)):
-                    os.makedirs(os.path.dirname(f))
-                bucket.download_file(f, f)
-                print(f)
+            transfer(download, fs)
 
         except Exception as e:
             print_err(e, 'download')
