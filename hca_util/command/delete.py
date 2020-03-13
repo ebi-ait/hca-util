602b1d4fd95dbda2237a4dc6606c9ef27c01e218
Prabhat Totoo <prabhat@ebi.ac.uk>
2020-03-13 00:07:24 +0000

    completed delete cmd impl incl. bucket policy cleanup

--- hca_util/command/delete.py
+++ hca_util/command/delete.py
@@ -1,61 +1,89 @@
-from .command import HcaCmd
+import json
+from botocore.exceptions import ClientError
+from hca_util.local_state import get_selected_dir
+from hca_util.common import print_err
+from hca_util.command.dir import CmdDir
 
 
-class CmdDelete(HcaCmd):
+class CmdDelete:
     """
     user: both wrangler and contributor, though contributor can't delete folder
     aws resource or client used in command - s3 resource (bucket.objects/ obj.delete)
     """
 
-    def cmd_delete(self, argv):
-        if not self.setup_ok:
-            print(f'Setup failed: \nSee `help config` for help to configure your credentials')
-            return
+    def __init__(self, aws, args):
+        self.aws = aws
+        self.args = args
 
-        if self.selected_dir is None:
+    def run(self):
+
+        selected_dir = get_selected_dir()
+
+        if not selected_dir:
             print('No directory selected')
             return
 
         try:
-            prefix = self.selected_dir
-            s3_resource = self.aws.session.resource('s3')
-            bucket = s3_resource.Bucket(self.bucket_name)
+            s3_resource = self.aws.common_session.resource('s3')
+            bucket = s3_resource.Bucket(self.aws.bucket_name)
 
-            if len(argv) == 0:
-                # delete an entire directory
-                confirm = input(f'Confirm delete {self.selected_dir} and its content? Y/y to proceed: ')
-                if confirm == 'Y' or confirm == 'y':
+            if self.args.d:  # delete dir
+                if self.aws.is_contributor:
+                    print('You don\'t have permission to use this command')
+                    return
+
+                confirm = input(f'Confirm delete {selected_dir}? Y/y to proceed: ')
+
+                if confirm.lower() == 'y':
                     print('Deleting...')
 
-                    for obj in bucket.objects.filter(Prefix=prefix):
-                        print('Deleting ' + obj.key)
+                    for obj in bucket.objects.filter(Prefix=selected_dir):
+                        print(obj.key)
                         obj.delete()
 
-                    # TODO delete bucket policy for HCAContributer-folder permissions
+                    # delete bucket policy for HCAContributer-folder permissions
+                    delete_dir_perms_from_bucket_policy(s3_resource, self.aws.bucket_name, selected_dir)
 
-                    # reset selected dir
-                    self.selected_dir = None
-                    print('Selected dir: None')
-
-                else:
-                    print('Delete cancelled')
-
+                    # clear selected dir
+                    CmdDir.clear(False)
                 return
 
-            if len(argv) == 1 and argv[0] == '.':
-                # delete all files in selected directory
-                for obj in bucket.objects.filter(Prefix=prefix):
+            if self.args.a:  # delete all files
+                print('Deleting...')
+                for obj in bucket.objects.filter(Prefix=selected_dir):
                     # do not delete folder object
-                    if obj.key == prefix:
+                    if obj.key == selected_dir:
                         continue
-                    print('Deleting ' + obj.key)
+                    print(obj.key)
                     obj.delete()
-            else:
-                # delete specified file(s) in selected directory
-                for f in argv:
-                    print('Deleting ' + prefix + f)
-                    obj = bucket.Object(prefix + f)
+                return
+
+            if self.args.f:  # delete list of file(s)
+                print('Deleting...')
+                for f in self.args.f:
+                    print('Deleting ' + selected_dir + f)
+                    obj = bucket.Object(selected_dir + f)
                     obj.delete()
+                return
 
         except Exception as e:
-            print(f'An exception of type {e.__class__.__name__} occurred in cmd delete.\nDetail: ' + str(e))
+            print_err(e, 'delete')
+
+
+def delete_dir_perms_from_bucket_policy(s3_res, bucket_name, dir_name):
+    try:
+        bucket_policy = s3_res.BucketPolicy(bucket_name)
+        policy_str = bucket_policy.policy
+    except ClientError:
+        policy_str = ''
+
+    if policy_str:
+        policy_json = json.loads(policy_str)
+        changed = False
+        for stmt in policy_json['Statement']:
+            if dir_name in stmt['Resource']:
+                policy_json['Statement'].remove(stmt)
+                changed = True
+        if changed:
+            updated_policy = json.dumps(policy_json)
+            bucket_policy.put(Policy=updated_policy)
