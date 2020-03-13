2cf4c7b698c24e9b71c3f3e78bbe9c34edf08bd7
prabht <prabhat.totoo@gmail.com>
2020-03-13 17:40:35 +0000

    replaced double with single quotes

--- setup.py
+++ setup.py
@@ -6,39 +6,39 @@ from setuptools import setup
 HERE = pathlib.Path(__file__).parent
 
 # text of the README file
-README = (HERE / "README.md").read_text()
+README = (HERE / 'README.md').read_text()
 
 # install requirements
-INSTALL_REQS = [line.rstrip() for line in open(os.path.join(os.path.dirname(__file__), "requirements.txt"))]
+INSTALL_REQS = [line.rstrip() for line in open(os.path.join(os.path.dirname(__file__), 'requirements.txt'))]
 
 # This call to setup() does all the work
 setup(
     # dashes are ok in repo and PyPI dist names but not in package (i.e. directory) and
     # module (.py file) names. can't do import xyz-abc
-    name="hca-util",
-    version="0.1.0",
-    description="CLI tool for file transfer (upload and download) to/from AWS S3.",
+    name='hca-util',
+    version='0.1.0',
+    description='CLI tool for file transfer (upload and download) to/from AWS S3.',
     long_description=README,
-    long_description_content_type="text/markdown",
-    url="https://github.com/ebi-ait/hca-util",
-    author="hca-ingest-dev",
-    author_email="prabhat@ebi.ac.uk",
-    license="Apache License",
+    long_description_content_type='text/markdown',
+    url='https://github.com/ebi-ait/hca-util',
+    author='hca-ingest-dev',
+    author_email='prabhat@ebi.ac.uk',
+    license='Apache License',
     classifiers=[
-        "License :: OSI Approved :: Apache Software License",
-        "Operating System :: MacOS :: MacOS X",
-        "Operating System :: POSIX",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
     platforms=['MacOS X', 'Posix'],
-    packages=["hca_util"],
+    packages=['hca_util'],
     include_package_data=True,
     install_requires=INSTALL_REQS,
     entry_points={
-        "console_scripts": [
-            "hca-util=hca_util.__main__:main",
+        'console_scripts': [
+            'hca-util=hca_util.__main__:main',
         ]
     },
 )
