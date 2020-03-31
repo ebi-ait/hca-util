2511caaff771e62069db754bcbffe0827dcb2750
Alegria Aclan <aaclan@ebi.ac.uk>
2020-03-31 16:23:54 +0100

    Added more tests for create command

--- tests/test_create.py
+++ tests/test_create.py
@@ -1,45 +1,100 @@
 import unittest
-from unittest.mock import MagicMock, Mock
+from unittest.mock import MagicMock, Mock, patch
 
 from hca_util.__main__ import parse_args
 from hca_util.command.create import CmdCreate
 
 
 class TestCreate(unittest.TestCase):
+    def setUp(self) -> None:
+        self.aws_mock = MagicMock()
+
+        self.client = MagicMock()
+        self.client.put_object = Mock()
+
+        bucket_policy = MagicMock()
+        bucket_policy.policy = None
+
+        resource = MagicMock()
+        resource.BucketPolicy = Mock(return_value=bucket_policy)
+
+        session = MagicMock()
+        session.client = Mock(return_value=self.client)
+        session.resource = Mock(return_value=resource)
+
+        self.aws_mock.is_contributor = False
+        self.aws_mock.common_session = session
+        self.aws_mock.bucket_name = 'bucket-name'
+
     def test_create_upload_area_no_config(self):
+        # given
         args = ['create', 'testUploadArea']
+
+        # when
         success, msg = CmdCreate(None, parse_args(args)).run()
+
+        # then
         self.assertFalse(success)
         self.assertEqual(msg, 'You need configure your profile first')
 
-    def test_create_upload_area_has_valid_config_for_contributor(self):
-        aws_mock = MagicMock()
-        aws_mock.is_contributor = True
-        # create upload area
+    def test_user_create_upload_area_has_valid_config(self):
+        # given
+        self.aws_mock.is_contributor = True
+
         args = ['create', 'testUploadArea']
-        success, msg = CmdCreate(aws_mock, parse_args(args)).run()
+
+        # when
+        success, msg = CmdCreate(self.aws_mock, parse_args(args)).run()
+
+        # then
         self.assertFalse(success)
         self.assertEqual(msg, 'You don\'t have permission to use this command')
 
-    def test_create_upload_area_has_valid_config_for_admin(self):
-        aws_mock = MagicMock()
-        session = MagicMock()
-        client = MagicMock()
-        resource = MagicMock()
-        bucket_policy = MagicMock()
+    @patch('uuid.uuid4')
+    def test_admin_create_upload_area(self, uuid):
+        # given
+        uuid.return_value = 'uuid'
 
-        client.put_object = Mock()
-        resource.BucketPolicy = Mock(return_value=bucket_policy)
-        bucket_policy.policy = None
-
-        session.client = Mock(return_value=client)
-        session.resource = Mock(return_value=resource)
-
-        aws_mock.is_contributor = False
-        aws_mock.common_session = session
-        aws_mock.bucket_name = 'bucket-name'
-        # create upload area
         args = ['create', 'testUploadArea']
-        success, msg = CmdCreate(aws_mock, parse_args(args)).run()
 
+        # when
+        success, msg = CmdCreate(self.aws_mock, parse_args(args)).run()
+
+        # then
         self.assertTrue(success)
+
+        metadata = {'name': 'testUploadArea', 'perms': 'ux'}
+        self.client.put_object.assert_called_once_with(Bucket='bucket-name', Key='uuid/', Metadata=metadata)
+
+    @patch('uuid.uuid4')
+    def test_admin_create_upload_area_with_permissions(self, uuid):
+        # given
+        uuid.return_value = 'uuid'
+        upload_area_name = 'testUploadArea'
+        permission = 'udx'
+
+        args = ['create', upload_area_name, '-p', permission]
+
+        # when
+        success, msg = CmdCreate(self.aws_mock, parse_args(args)).run()
+
+        # then
+        self.assertTrue(success)
+
+        metadata = {'name': upload_area_name, 'perms': permission}
+        self.client.put_object.assert_called_once_with(Bucket='bucket-name', Key='uuid/', Metadata=metadata)
+
+    @patch('uuid.uuid4')
+    def test_admin_create_upload_area_no_name(self, uuid):
+        # given
+        uuid.return_value = 'uuid'
+        args = ['create', 'testUploadArea']
+
+        # when
+        success, msg = CmdCreate(self.aws_mock, parse_args(args)).run()
+
+        # then
+        self.assertTrue(success)
+
+        metadata = {'name': 'testUploadArea', 'perms': 'ux'}
+        self.client.put_object.assert_called_once_with(Bucket='bucket-name', Key='uuid/', Metadata=metadata)
