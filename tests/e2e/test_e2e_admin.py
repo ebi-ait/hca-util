d29d7a343195a5730c6b201e2b588770e716cb8c
Alegria Aclan <aaclan@ebi.ac.uk>
2020-04-01 17:14:36 +0100

    Refactor e2e test

--- tests/e2e/test_e2e_admin.py
+++ tests/e2e/test_e2e_admin.py
@@ -10,7 +10,7 @@ admin_access = os.environ.get('HCA_UTIL_ADMIN_ACCESS')
 admin_secret = os.environ.get('HCA_UTIL_ADMIN_SECRET')
 
 
-def run(command: str, input: str = None, verbose=True):
+def run(command: str, input: str = None, verbose: bool = True):
     parsed_command = command.split(' ')
     proc = subprocess.Popen(parsed_command,
                             stdin=subprocess.PIPE,
@@ -39,64 +39,50 @@ def search_uuid(text: str):
 
 class TestAdminE2E(TestCase):
     def test_e2e_admin(self):
-        # config
-        print('Configuring covid-util')
-        config, output, error = run(f'covid-util config {admin_access} {admin_secret} --profile covid-util', verbose=False)
-        print(f'{output}')
-        self.assertEqual(config, 0, f'config is not successful, output: {output}, error:{error}')
+        profile = '--profile covid-util'
 
-        # create upload area
-        print('Creating Upload Area')
+        print('# Configuring covid-util\n')
+        self._assert_successful_run(f'covid-util config {admin_access} {admin_secret} {profile}', verbose=False)
+
+        print('# Creating Upload Area\n')
         upload_area = 'testuploadarea'
-        create_upload, output, error = run(f'covid-util create {upload_area} --profile covid-util')
+        output = self._assert_successful_run(f'covid-util create {upload_area} {profile}')
 
-        self.assertEqual(create_upload, 0, f'upload area {upload_area} is not created, output: {output}, error:{error}')
         upload_area_uuid = search_uuid(output)
         self.assertTrue(upload_area_uuid, 'The upload area uuid could not be found from the output')
 
-        # select upload area
-        print('Selecting Upload Area')
-        select_upload, output, error = run(f'covid-util select {upload_area_uuid} --profile covid-util')
-        self.assertEqual(select_upload, 0,
-                         f'upload area {upload_area_uuid} is not selected, output: {output}, error:{error}')
+        print('# Selecting Upload Area\n')
+        self._assert_successful_run(f'covid-util select {upload_area_uuid} {profile}')
 
-        # create tests file
-        content = 'testing uploading of file'
         filename = 'tests-file.txt'
+        self._assert_successful_run(f'touch {filename}')
 
-        create_file, output, error = run(f'touch {filename}')
-        self.assertEqual(create_file, 0, f'Creation of tests file failed, output: {output}, error:{error}')
+        print('# Uploading file\n')
+        self._assert_successful_run(f'covid-util upload -f {filename} {profile}')
 
-        # upload tests file
-        print('Uploading file')
-        upload_file, output, error = run(f'covid-util upload -f {filename} --profile covid-util')
-        self.assertEqual(upload_file, 0, f'file upload failed, output: {output}, error:{error}')
+        print('# Listing file\n')
+        output = self._assert_successful_run(f'covid-util list {profile}')
+        self.assertTrue(filename in output, f'file {filename} was not uploaded to {upload_area}, output: {output}')
 
-        # list files
-        print('Listing file')
-        list_files, output, error = run(f'covid-util list --profile covid-util')
-        self.assertEqual(list_files, 0, f'file cannot be listed, output: {output}, error:{error}')
-        self.assertTrue(filename in output,
-                        f'file {filename} was not uploaded to {upload_area}, output: {output}, error:{error}')
+        print('# Deleting file\n')
+        self._assert_successful_run(f'covid-util delete -f {filename} {profile}')
 
-        # delete file
-        print('Deleting file')
-        delete_file, output, _ = run(f'covid-util delete -f {filename} --profile covid-util')
-        self.assertEqual(delete_file, 0, f'{filename} cannot be deleted, output: {output}, error:{error}')
+        print('# Listing file to check if it is deleted\n')
+        output = self._assert_successful_run(f'covid-util list {profile}')
+        self.assertFalse(filename in output, f'file {filename} was not deleted to {upload_area}, output: {output}')
 
-        # list files
-        print('Listing file to check if it is deleted')
-        list_files, output, error = run(f'covid-util list --profile covid-util')
-        self.assertFalse(filename in output,
-                         f'file {filename} was not deleted to {upload_area}, output: {output}, error:{error}')
+        print('# Deleting upload area\n')
+        self._assert_successful_run(f'covid-util delete -d {profile}', input="y\n")
 
-        print('Deleting upload area')
-        delete_upload, output, _ = run(f'covid-util delete -d --profile covid-util', "y\n")
-
-        print('Listing upload area to check if it is deleted')
-        select_upload, output, error = run(f'covid-util select {upload_area_uuid} --profile covid-util')
+        print('# Listing upload area to check if it is deleted\n')
+        select_upload, output, error = run(f'covid-util select {upload_area_uuid} {profile}')
         self.assertEqual(select_upload, 1,
                          f'upload area {upload_area_uuid} is not deleted, output: {output}, error:{error}')
 
-        delete_local_file, output, error = run(f'rm {filename}')
-        self.assertEqual(delete_local_file, 0, f'{filename} cannot be deleted locally, output: {output}, error:{error}')
+        # cleanup
+        self._assert_successful_run(f'rm {filename}')
+
+    def _assert_successful_run(self, command: str, **kwargs):
+        exit_code, output, error = run(command, **kwargs)
+        self.assertEqual(exit_code, 0, f'output: {output}, error:{error}')
+        return output
