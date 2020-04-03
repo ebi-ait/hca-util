1a2b46289fe6a21c737bb38bb39594030f163f04
Alegria Aclan <aaclan@ebi.ac.uk>
2020-04-03 11:46:56 +0100

    Added script to delete upload areas specified in a file

---
+++ scripts/delete_upload_areas.py
@@ -0,0 +1,19 @@
+import sys
+
+from tests.e2e.test_utils import run, search_all_uuids
+
+FILENAME = sys.argv[1]  # file which contains uuids to delete
+
+CLI = 'covid-util'
+
+code, output, error = run(FILENAME)
+uuids = search_all_uuids(output)
+
+print(uuids)
+
+print(len(uuids))
+
+for uuid in uuids:
+    run(f'{CLI} select {uuid}')
+    run(f'{CLI} delete -d', "y\n")
+    print(f'deleted {uuid}!')
