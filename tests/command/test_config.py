bdfce329430ad4aacea6e989ea5541a3c0b7c5b3
Alegria Aclan <aaclan@ebi.ac.uk>
2020-04-07 13:46:26 +0100

    Added more tests for config command

--- tests/command/test_config.py
+++ tests/command/test_config.py
@@ -1,10 +1,10 @@
 import os
 import unittest
+from unittest.mock import patch
 
+from settings import IAM_USER, IAM_ADMIN
 from util.__main__ import parse_args
 from util.command.config import CmdConfig
-from settings import IAM_USER, IAM_ADMIN
-
 
 user_access = os.environ.get('USER_ACCESS')
 user_secret = os.environ.get('USER_SECRET')
@@ -20,14 +20,50 @@ class TestConfig(unittest.TestCase):
         self.assertEqual(msg, 'Valid credentials')
         self.assertTrue(success)
 
+    def test_config_invalid_user_creds(self):
+        args = ['config', 'invalid-key', 'invalid-secret', '--profile', IAM_USER]
+        success, msg = CmdConfig(parse_args(args)).run()
+        self.assertEqual(msg, 'Invalid credentials')
+        self.assertFalse(success)
+
     def test_config_valid_admin_creds(self):
         args = ['config', admin_access, admin_secret, '--profile', IAM_ADMIN]
         success, msg = CmdConfig(parse_args(args)).run()
         self.assertTrue(success)
         self.assertEqual(msg, 'Valid credentials')
 
+    def test_config_invalid_admin_creds(self):
+        args = ['config', 'invalid-key', 'invalid-secret', '--profile', IAM_ADMIN]
+        success, msg = CmdConfig(parse_args(args)).run()
+        self.assertEqual(msg, 'Invalid credentials')
+        self.assertFalse(success)
+
     def test_config_invalid_creds(self):
         args = ['config', 'invalid-key', 'invalid-secret']
         success, msg = CmdConfig(parse_args(args)).run()
         self.assertFalse(success)
         self.assertEqual(msg, 'Invalid credentials')
+
+    def test_config_valid_user_creds(self):
+        args = ['config', user_access, user_secret]
+        success, msg = CmdConfig(parse_args(args)).run()
+        self.assertTrue(success)
+        self.assertEqual(msg, 'Valid credentials')
+
+    def test_config_valid_admin_creds(self):
+        args = ['config', admin_access, admin_secret]
+        success, msg = CmdConfig(parse_args(args)).run()
+        self.assertTrue(success)
+        self.assertEqual(msg, 'Valid credentials')
+
+    def test_config_missing_params(self):
+        args = ['config', 'any-key']
+        with self.assertRaises(SystemExit) as error:
+            success, msg = CmdConfig(parse_args(args)).run()
+            self.assertFalse(success)
+
+    @patch('util.command.config.set_bucket')
+    def test_config_with_bucket(self, set_bucket):
+        args = ['config', user_access, user_secret, '--bucket', 'bucket']
+        success, msg = CmdConfig(parse_args(args)).run()
+        self.assertTrue(success)
