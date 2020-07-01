4264e7597cc982495d81df48bfdee1521a27a8c4
prabh-t <prabhat@ebi.ac.uk>
2020-07-02 00:41:00 +0100

    add s3:GetObjectTagging to user object policy

--- ait/commons/util/bucket_policy.py
+++ ait/commons/util/bucket_policy.py
@@ -1,29 +1,31 @@
 from ait.commons.util.settings import AWS_ACCOUNT, IAM_USER
 
 """
-Permissions include
-u for upload
-d for download
-x for delete
-Possible combinations are 'u', 'ud', 'ux', 'udx'
-default permissions on new directory is 'ux'
+Upload area permissions
+Abbrev  Desc.       S3 action           S3 operation/API request
+                    (object ops perms)
+u       upload      s3:PutObject        # PUT Object, POST Object, Initiate Multipart Upload, Upload Part, Complete Multipart Upload, PUT Object - Copy
+d       download    s3:GetObject        # GET Object, HEAD Object, SELECT Object Content
+x       delete      s3:DeleteObject     # DELETE Object
 
+Possible combinations are 'u', 'ud', 'ux', 'udx'
+
+Default permissions on new upload area is 'ux'
+
+User groups:
+admin           (at the moment) full s3 access
+(normal) user   default: ListBucket except top/root level dir, plus per upload area perms (one of above combination), plus s3:GetObjectTagging
 """
+
 ALLOWED_PERMS = ['u', 'ud', 'ux', 'udx']
 DEFAULT_PERMS = 'ux'
 
-# user groups:
-# admin     (normally) s3 full access
-# user      util bucket (ListBucket except top-level/root directory)
-
-# Permissions for Object operations
-#                       permissions        s3 operations
-s3_permissions = {'u': 's3:PutObject',     # PUT Object, POST Object, Initiate Multipart Upload, Upload Part, Complete Multipart Upload, PUT Object - Copy
-                  'd': 's3:GetObject',     # GET Object, HEAD Object, SELECT Object Content
-                  'x': 's3:DeleteObject'}  # DELETE Object
+s3_permissions = {'u': 's3:PutObject',
+                  'd': 's3:GetObject',
+                  'x': 's3:DeleteObject'}
 
 
-def get_policy_statement_template():
+def user_policy_statement_template():
     return {
         'Action': [],
         'Effect': 'Allow',
@@ -37,12 +39,14 @@ def get_policy_statement_template():
 
 
 def new_policy_statement(bucket, area, perms=None):
-    statement = get_policy_statement_template()
+    statement = user_policy_statement_template()
     # add permissions/actions
     if perms is None or perms not in ALLOWED_PERMS:
         perms = DEFAULT_PERMS
     for p in perms:
         statement['Action'].append(s3_permissions.get(p))
-        res = statement['Resource']
-        statement['Resource'] = res.replace('BUCKET_NAME', bucket).replace('AREA_NAME', area)
+    # additional action needed for user to see tagging (area name and perms)
+    statement['Action'].append("s3:GetObjectTagging")
+    res = statement['Resource']
+    statement['Resource'] = res.replace('BUCKET_NAME', bucket).replace('AREA_NAME', area)
     return statement
