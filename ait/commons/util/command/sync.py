5458ac930cc9fbab9cb15488326a675c4d82daad
prabh-t <prabhat@ebi.ac.uk>
2020-07-23 14:29:16 +0100

    improves and fixes the issues with sync cmd

--- ait/commons/util/command/sync.py
+++ ait/commons/util/command/sync.py
@@ -1,14 +1,18 @@
 import os
 import json
 import requests
+from tqdm import tqdm
+from multiprocessing import cpu_count
+from multiprocessing.pool import ThreadPool
+import random
 
 from botocore.exceptions import ClientError
 
 from ait.commons.util.aws_client import Aws
 from ait.commons.util.bucket_policy import new_policy_statement
-from ait.commons.util.common import gen_uuid, format_err, INGEST_UPLOAD_AREA_PREFIX
-from ait.commons.util.file_transfer import FileTransfer, TransferProgress, transfer
+from ait.commons.util.common import gen_uuid, format_err, INGEST_UPLOAD_AREA_PREFIX, sizeof_fmt
 from ait.commons.util.local_state import get_selected_area
+
 
 UPLOAD_SERVICE_API = 'https://upload__ENV__archive.data.humancellatlas.org'
 
@@ -40,70 +44,82 @@ class CmdSync:
         dest_bucket, dest_env, dest_upload_area_uuid = self.args.INGEST_UPLOAD_AREA
 
         try:
+            # Resources are not thread safe.
+            # Low-level clients are thread safe. When using a low-level client, it is recommended to instantiate 
+            # your client then pass that client object to each of your threads.
 
-            s3_resource = self.aws.common_session.resource('s3')
-            bucket = s3_resource.Bucket(self.aws.bucket_name)
+            s3 = self.aws.common_session.resource('s3')
+            bucket = s3.Bucket(self.aws.bucket_name)
 
             fs = []
+            total_size = 0
 
             # get all files from selected area
             for obj in bucket.objects.filter(Prefix=selected_area):
                 # skip the top-level directory
                 if obj.key == selected_area:
                     continue
-                contentType = obj.get().get('ContentType')
-                # using path for contentType
-                fs.append(FileTransfer(path=contentType, key=obj.key, size=obj.size))
-                
-            def copy(idx):
+                total_size += obj.size
+                fs.append(obj)
+
+            failed_fs = []
+            
+            def transfer(f):
                 try:
-                    k = fs[idx].key
-                    s3 = self.aws.new_session().resource('s3')
+
+                    fname = f.key[37:]
+                    contentType = ''
+                    obj_ = s3.meta.client.head_object(Bucket=self.aws.bucket_name, Key=f.key)
+                    if obj_ and obj_['ContentType']:
+                        contentType = obj_['ContentType']
+
                     copy_source = {
                         'Bucket': self.aws.bucket_name,
-                        'Key': k
+                        'Key': f.key
                     }
-                    filename = k[37:]
+                    dest_key = dest_upload_area_uuid + '/' + fname
 
-                    dest_key = dest_upload_area_uuid + '/' + filename
                     s3.meta.client.copy(copy_source, dest_bucket, dest_key, 
-                                        Callback=TransferProgress(fs[idx]), 
-                                        ExtraArgs={'ContentType': fs[idx].path})
-                    
-                    # if file size is 0, callback will likely never be called
-                    # and complete will not change to True
-                    # hack
-                    if fs[idx].size == 0:
-                        fs[idx].status = 'Empty file.'
-                        fs[idx].complete = True
-                        fs[idx].successful = True
+                                    Callback=pbar.update, 
+                                    ExtraArgs={'ContentType': contentType})
 
-                    if notify_upload(dest_env, dest_upload_area_uuid, filename):
-                        fs[idx].status = 'Transferred. Notify failed.'
-                        fs[idx].complete = True
-                        fs[idx].successful = False
+                    if not notify_upload(dest_env, dest_upload_area_uuid, fname):
+                        failed_fs.append((f, 'Transferred. Notify failed.'))
 
+                except ClientError as ex:
+                    if ex.response['Error']['Code'] == 'NoSuchKey':
+                        failed_fs.append((f, 'NoSuchKey'))
+                    else:
+                        failed_fs.append((f, str(thread_ex)))
+                    pass
 
                 except Exception as thread_ex:
-                    if 'Forbidden' in str(thread_ex) or 'AccessDenied' in str(thread_ex):
-                        fs[idx].status = 'Access denied.'
-                    else:
-                        fs[idx].status = 'Transfer failed.'
-                    fs[idx].complete = True
-                    fs[idx].successful = False
+                    failed_fs.append((f, str(thread_ex)))
+                    pass
 
             print('Transferring...')
-            transfer(copy, fs)
+            pbar = tqdm(total=total_size, unit='B', unit_scale=True, desc=num_files(fs))
+            pool = ThreadPool(cpu_count())
+            pool.map_async(transfer, fs)
+            pool.close()
+            pool.join()
+            pbar.close()
 
-            self.files = [f for f in fs if f.successful]
-
-            if all([f.successful for f in fs]):
-                return True, 'Transfer complete.'
+            if failed_fs:
+                print(f'{num_files(failed_fs)} failed to transfer: ')
+                for f,err in failed_fs:
+                    print(f'{f.key} {err}')
+                return False, 'Transfer complete with error.'
             else:
-                return False, 'Transfer error.'
+                return True, 'Transfer complete.'
             
         except Exception as e:
             return False, format_err(e, 'sync')
+
+
+def num_files(ls):
+    l = len(ls)
+    return f'{l} file{"s" if l > 1 else ""}'
 
 
 """
